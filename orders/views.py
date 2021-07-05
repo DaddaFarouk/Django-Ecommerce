744683f0baba01969a6e39d3ebf5e90bb2a8448a
DaddaFarouk <faroukdadda@gmail.com>
2021-07-05 11:16:33 +0100

    Payment Successful form

--- orders/views.py
+++ orders/views.py
@@ -1,4 +1,4 @@
-from django.http.response import HttpResponse
+from django.http.response import HttpResponse, JsonResponse
 from django.shortcuts import render,redirect
 from carts.models import CartItem
 from .forms import OrderForm
@@ -71,8 +71,13 @@ def payments(request): # Get the data from paypal
 
     # Send order number and transaction id back to sendData method via JsonResponse
 
+    data = {
+        'order_number': order.order_number,
+        'transID': payment.payment_id,
+    }
 
-    return render(request, 'orders/payments.html')
+
+    return JsonResponse(data) # Data will be returned to from where it came
 
 
 def place_order(request, total=0, quantity=0):
@@ -134,4 +139,34 @@ def place_order(request, total=0, quantity=0):
 
         else:
             return redirect('checkout')
+
+
+def order_complete(request):
+    order_number = request.GET.get('order_number')
+    transID      = request.GET.get('payment_id')
+    
+    try:
+        order = Order.objects.get(order_number=order_number, is_ordered=True)
+        ordered_products = OrderProduct.objects.filter(order_id=order.id)
+        
+        subtotal =0
+        for i in ordered_products:
+            subtotal += i.product_price * i.quantity
+
+        payment = Payment.objects.get(payment_id=transID)
+
+        context = {
+            'order': order,
+            'ordered_products': ordered_products,
+            'order_number': order.order_number,
+            'transID': payment.payment_id,
+            'payment': payment,
+            'subtotal': subtotal,
+        }
+
+        return render(request, 'orders/order_complete.html', context)
+    
+    except (Payment.DoesNotExist, Order.DoesNotExist): # If the order does not exist
+        return redirect('home')
+
 
